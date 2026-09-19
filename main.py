7ec5a6bc0b608210df58dbc7a1d60a8793f5e5f4
Oratile-KO <162906040+Oratile-KO@users.noreply.github.com>
2026-09-19 19:28:07 +0200

    Add contribution recording

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from datetime import date
+
 #Declare and Initialize the members list
 members = []
 contributions = []
 prefix  = 'M'
 suffix = 1
+today = date.today()
 
 #Create function to add member
 def add_member(prefix, suffix):
@@ -20,6 +23,17 @@ def view_member():
     for member in members:
         print(f"\n{member['member_id']} {member['name']}")
 
+#Record member contributions
+def record_contribution():
+    view_member()
+    member_contributing = input("Enter member ID: ").strip().capitalize()
+    amount = float(input(f"Enter amount member is contributing: ").strip()) 
+    for contribution in members:
+        if contribution['member_id'] == member_contributing:
+            new_contribution = {'name':contribution['name'], 'member_id': member_contributing, 'amount': amount, 'date': today.strftime("%d/%b/%Y")}
+            contributions.append(new_contribution)
+    print(contributions) #Testing purposes,remove later
+
 while True:
     #Prompt user to select option from the menu
     option = int(input(f"\nSelect an option: \n1. Add member \n2. View members \n3. Record contribution \n4. View contributions \n5. Calculate member total \n6. Calculate group total\n").strip())
@@ -34,3 +48,11 @@ while True:
             print("-", end='')
             i += 1
         view_member()
+
+    elif option == 3:
+        print("Available members:")
+        i = 0
+        while i < 20:
+            print("-", end='')
+            i += 1
+        record_contribution()
