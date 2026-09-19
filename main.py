517d8f37fdfee1f7393a32d924e336411493f65a
Oratile-KO <162906040+Oratile-KO@users.noreply.github.com>
2026-09-19 18:34:53 +0200

    Add member listing

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def add_member(prefix, suffix):
     members.append(new_member)
     print(f"{name} added successfully! Member ID: {member_id}\n")
 
+#Display the members and their member ID's
+def view_member():
+    for member in members:
+        print(f"\n{member['member_id']} {member['name']}")
+
 while True:
     #Prompt user to select option from the menu
     option = int(input(f"\nSelect an option: \n1. Add member \n2. View members \n3. Record contribution \n4. View contributions \n5. Calculate member total \n6. Calculate group total\n").strip())
+
+    if option == 1:
+        add_member(prefix, suffix)
+
+    elif option == 2:
+        print("Members")
+        i = 0
+        while i < 20:
+            print("-", end='')
+            i += 1
+        view_member()
